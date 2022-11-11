1da49b31e266cc11d4507023c53d193ff5213029
Matheus Clemente <matheus.xb@gmail.com>
2022-11-11 15:03:42 -0300

    Prettier changes

--- Macros.js
+++ Macros.js
@@ -69,12 +69,7 @@ class FurnaceMacros {
 		libWrapper.register("advanced-macros", "Macro.prototype._executeScript", executeScript, "OVERRIDE");
 		libWrapper.register("advanced-macros", "Macro.prototype.execute", this.executeMacro, "OVERRIDE");
 		if (game.system.id === "pf2e") {
-			libWrapper.register(
-				"advanced-macros",
-				"CONFIG.Macro.documentClass.prototype._executeScript",
-				executeScript,
-				"OVERRIDE"
-			);
+			libWrapper.register("advanced-macros", "CONFIG.Macro.documentClass.prototype._executeScript", executeScript, "OVERRIDE");
 		}
 	}
 	ready() {
