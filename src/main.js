be16b733f99651566290260689c590c77baf2fa3
Matheus Clemente <matheus.xb@gmail.com>
2023-05-15 04:57:28 -0300

    Merge everything a single file

--- src/main.js
+++ src/main.js
@@ -2,22 +2,13 @@
  * Author: Matheus Clemente (https://github.com/mclemente) & 4535992 (https://github.com/p4535992)
  * Software License: MIT
  */
-import {
-	_createContentLink,
-	_onClickContentLink,
-	canRunAsGM,
-	chatMessage,
-	executeMacro,
-	renderMacroConfig,
-} from "./scripts/logic.js";
-import { error } from "./scripts/utils.js";
 
-export let socket;
+let socket;
 Hooks.once("socketlib.ready", () => {
 	//@ts-ignore
 	socket = socketlib.registerModule("advanced-macros");
 	socket.register("executeMacro", async (...inAttributes) => {
-		if (!Array.isArray(inAttributes)) throw error("executeMacroArr | inAttributes must be of type array");
+		if (!Array.isArray(inAttributes)) throw error("executeMacroArr: inAttributes must be of type array");
 		let [macro, args] = inAttributes;
 		macro = game.macros.getName(macro.name);
 		return macro?.execute(args, true);
@@ -46,55 +37,168 @@ Hooks.once("init", () => {
 		"OVERRIDE",
 	);
 
-	libWrapper.register("advanced-macros", "Macro.prototype.execute", executeMacro, "MIXED");
-
 	libWrapper.register(
 		"advanced-macros",
-		"ChatLog.prototype._processMacroCommand",
-		function (command, match) {
-			let [macroName, ...params] = match[2].split(" ");
-			let expandName = true;
+		"Macro.prototype.execute",
+		async function (wrapped, scope, callFromSocket) {
+			if (!this.canExecute) {
+				return ui.notifications.warn(`You do not have permission to execute Macro "${this.name}".`);
+			}
+			switch (this.type) {
+				case "chat":
+					return wrapped();
+				case "script":
+					const runFor = this.getFlag("advanced-macros", "runForSpecificUser");
+					if (callFromSocket || !runFor || !canRunAsGM(this)) return wrapped(scope);
+					if (runFor == "GM") return socket.executeAsGM("executeMacro", this, scope);
+					else if (runFor == "runForEveryone") return socket.executeForEveryone("executeMacro", this, scope);
+					else if (runFor == "runForEveryoneElse")
+						return socket.executeForOthers("executeMacro", this, scope);
+					else if (this.getFlag("advanced-macros", "runForSpecificUser")) {
+						const users = [this.getFlag("advanced-macros", "runForSpecificUser")];
+						return socket.executeForUsers("executeMacro", users, this, scope);
+					}
+			}
+		},
+		"MIXED",
+	);
+	libWrapper.register(
+		"advanced-macros",
+		"TextEditor._createContentLink",
+		(match, { async = false, relativeTo } = {}) => {
+			let [type, target, hash, name] = match.slice(1, 5);
+
+			// Prepare replacement data
+			const data = {
+				cls: ["content-link"],
+				icon: null,
+				dataset: {},
+				name,
+			};
+
+			let doc;
+			let broken = false;
 			const scope = {};
-
-			// Get the target macro by number or by name
-			let macro;
-			if (Number.isNumeric(macroName)) {
-				const macroID = game.user.hotbar[macroName];
-				macro = game.macros.get(macroID);
-			}
-			if (!macro) macro = game.macros.getName(macroName);
-
-			for (const p of params) {
-				const kv = p.split("=");
-				if (kv.length === 2) {
-					scope[kv[0]] = kv[1];
-					expandName = false;
-				} else if (macro) {
-					scope[kv[0]] = true;
-					expandName = false;
-				} else if (expandName) {
-					macroName += ` ${p}`; // Macro names may contain spaces
-					macro = game.macros.getName(macroName);
+			if (type === "UUID") {
+				if (target.split(".")[0] == "Macro" && /\s/g.test(match[4])) {
+					const macro = game.macros.get(target.split(".")[1]);
+					let params = match[4].replace(`${macro.name} `, "").split(" ");
+					for (const p of params) {
+						const kv = p.split("=");
+						if (kv.length === 2) {
+							scope[kv[0]] = kv[1];
+						}
+					}
+					data.name = macro.name;
 				}
-			}
-			if (!macro) {
-				macroName = macroName.trimEnd(); // Eliminate trailing spaces
-				macro = game.macros.getName(macroName);
+				data.dataset = { id: null, uuid: target, scope };
+				if (async) doc = fromUuid(target, { relative: relativeTo });
+				else {
+					try {
+						doc = fromUuidSync(target, { relative: relativeTo });
+					} catch (err) {
+						[type, ...target] = target.split(".");
+						broken = TextEditor._createLegacyContentLink(type, target.join("."), name, data);
+					}
+				}
+			} else broken = TextEditor._createLegacyContentLink(type, target, name, data);
+
+			// Flag a link as broken
+			if (broken) {
+				data.icon = "fas fa-unlink";
+				data.cls.push("broken");
 			}
 
-			if (!macro)
-				throw new Error(`Requested Macro "${macroName}" was not found as a named macro or hotbar position`);
+			const constructAnchor = (doc) => {
+				if (doc) {
+					if (doc.documentName) {
+						const attrs = { draggable: true };
+						if (hash) attrs["data-hash"] = hash;
+						return doc.toAnchor({
+							attrs,
+							dataset: { scope: Object.keys(data.dataset.scope), ...data.dataset.scope },
+							classes: data.cls,
+							name: data.name,
+						});
+					}
+					data.name = data.name || doc.name || target;
+					const type = game.packs.get(doc.pack)?.documentName;
+					data.dataset.type = type;
+					data.dataset.id = doc._id;
+					data.dataset.pack = doc.pack;
+					if (hash) data.dataset.hash = hash;
+					data.icon = CONFIG[type].sidebarIcon;
+				} else if (type === "UUID") {
+					// The UUID lookup failed so this is a broken link.
+					data.icon = "fas fa-unlink";
+					data.cls.push("broken");
+				}
 
-			// Execute the Macro with provided scope
-			return macro.execute(scope);
+				const a = document.createElement("a");
+				a.classList.add(...data.cls);
+				a.draggable = true;
+				for (let [k, v] of Object.entries(data.dataset)) {
+					a.dataset[k] = v;
+				}
+				a.innerHTML = `<i class="${data.icon}"></i>${data.name}`;
+				return a;
+			};
+
+			if (doc instanceof Promise) return doc.then(constructAnchor);
+			return constructAnchor(doc);
 		},
 		"OVERRIDE",
 	);
-	libWrapper.register("advanced-macros", "TextEditor._createContentLink", _createContentLink, "OVERRIDE");
-	libWrapper.register("advanced-macros", "TextEditor._onClickContentLink", _onClickContentLink, "OVERRIDE");
+	libWrapper.register(
+		"advanced-macros",
+		"TextEditor._onClickContentLink",
+		async (event) => {
+			event.preventDefault();
+			const doc = await fromUuid(event.currentTarget.dataset.uuid);
+			if (event.currentTarget.dataset.type !== "Macro") return doc?._onClickDocumentLink(event);
+			else {
+				const scope = {};
+				if (event.currentTarget.dataset.scope) {
+					const params = event.currentTarget.dataset.scope.split(",");
+					for (const p of params) {
+						scope[p] =
+							event.currentTarget.dataset[p] === "true" ||
+							(event.currentTarget.dataset[p] === "false" ? false : event.currentTarget.dataset[p]);
+					}
+				}
+				// TODO set up logic check when to callFromSocket or not
+				return doc?.execute(scope);
+			}
+		},
+		"OVERRIDE",
+	);
 });
 
-Hooks.on("chatMessage", chatMessage);
+Hooks.on("chatMessage", (chatLog, message, chatData) => {
+	// Ignore messages starting with "<" or matching a macro pattern.
+	if (message.trim().startsWith("<") || message.match(chatLog.constructor.MESSAGE_PATTERNS["macro"])) return true;
+	if (!game.settings.get("advanced-macros", "legacySlashCommand")) return;
+	// If the message contains an invalid command and starts with a "/", try to process macros in it.
+	let [command, match] = chatLog.constructor.parse(message);
+	if (command === "invalid" && message.trim().startsWith("/")) {
+		const messageArray = message.slice(1).split(" ");
+		let macroName = messageArray[0];
+		let macro = game.macros.getName(macroName);
+		messageArray.slice(1).forEach((token) => {
+			if (!macro) {
+				macroName += ` ${token}`;
+				macro = game.macros.getName(macroName);
+			}
+			if (macro) return;
+		});
+		if (macro) {
+			[command, match] = chatLog.constructor.parse(`/macro ${message.slice(1)}`);
+			chatLog._processMacroCommand(command, match);
+			return false;
+		}
+	}
+	return true;
+});
 
 Hooks.once("ready", () => {
 	if (!game.modules.get("lib-wrapper")?.active && game.user?.isGM) {
@@ -103,6 +207,72 @@ Hooks.once("ready", () => {
 		throw error(`Requires the 'libWrapper' module. Please ${word} it.`);
 	}
 	Hooks.on("renderMacroConfig", (obj, html, data) => {
-		renderMacroConfig(obj, html, data);
+		if (!game.user.isGM) return;
+		const macro = obj.object;
+		// A re-render will cause the html object to be the internal element, which is the form itself.
+		const form = html.find("form").length === 0 ? html : html.find("form");
+		const typeGroup = form.find("select[name=type]").parent(".form-group");
+		const runForSpecificUser = macro.getFlag("advanced-macros", "runForSpecificUser");
+		const options = [
+			{ value: "", label: game.i18n.localize("advanced-macros.MACROS.none") },
+			{ value: "GM", label: game.i18n.localize("advanced-macros.MACROS.runAsGM") },
+			{ value: "runForEveryone", label: game.i18n.localize("advanced-macros.MACROS.runForEveryone") },
+			{ value: "runForEveryoneElse", label: game.i18n.localize("advanced-macros.MACROS.runForEveryoneElse") },
+			...game.users
+				.filter((user) => !user.isGM)
+				.map((user) => ({
+					value: user.id,
+					label: user.name,
+					selected: runForSpecificUser === user.id,
+				})),
+		]; // filter out null values
+
+		const optionElements = options
+			.map((option) => {
+				const selectedAttr = option.selected ? 'selected="selected"' : "";
+				return `<option value="${option.value}" ${selectedAttr}>${option.label}</option>`;
+			})
+			.join("");
+
+		const specificOneDiv = $(`
+			<div class="form-group"
+				${macro.type === "chat" ? 'style="display: none"' : ""}
+				title="${game.i18n.localize("advanced-macros.MACROS.runForSpecificUserTooltip")}">
+				<label>${game.i18n.localize("advanced-macros.MACROS.runForSpecificUser")}</label>
+				<select name="flags.advanced-macros.runForSpecificUser" ${canRunAsGM(macro) ? "" : "disabled"}>
+					${optionElements}
+				</select>
+			</div>
+		`);
+
+		specificOneDiv.insertAfter(typeGroup);
+
+		const typeSelect = form.find("select[name=type]");
+		typeSelect.on("change", (event) => {
+			if (event.target.value === "chat") specificOneDiv.hide();
+			else specificOneDiv.show();
+		});
 	});
 });
+
+/**
+ * Defines whether a Macro can run as a GM.
+ * For security reasons, only macros authored by the GM, and not editable by users
+ * can be run as GM
+ */
+function canRunAsGM(macro) {
+	const author = game.users.get(macro.author?.id);
+	const permissions = deepClone(macro.permission) || {};
+
+	for (const user of game.users.contents) {
+		if (user.isGM || user.id === author?.id) delete permissions[user.id];
+	}
+	const highestPermissionLevel = Math.max(...Object.values(permissions));
+	return author?.isGM && highestPermissionLevel < CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER;
+}
+
+function error(error, notify = true) {
+	const errorMsg = `Advanced Macros | ${error}`;
+	if (notify) ui.notifications?.error(errorMsg);
+	return new Error(errorMsg.replace(/\s*<br>\s*/g, "\n"));
+}
--- src/scripts/utils.js
+++
@@ -1,5 +0,0 @@
-export function error(error, notify = true) {
-	const errorMsg = `Advanced Macros | ${error}`;
-	if (notify) ui.notifications?.error(errorMsg);
-	return new Error(errorMsg.replace(/\s*<br>\s*/g, "\n"));
-}
