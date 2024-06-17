3f8f28c86e6fe579deb28ef8edf981fd93710f31
Matheus Clemente <matheus.xb@gmail.com>
2024-06-17 16:33:02 -0300

    V12 Update
    Fix #86

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@
 
 let socket;
 Hooks.once("socketlib.ready", () => {
-	//@ts-ignore
 	socket = socketlib.registerModule("advanced-macros");
 	socket.register("executeMacro", async (...inAttributes) => {
 		if (!Array.isArray(inAttributes)) {
@@ -18,76 +17,61 @@ Hooks.once("socketlib.ready", () => {
 });
 
 Hooks.once("init", () => {
-	game.settings.register("advanced-macros", "legacySlashCommand", {
-		name: `advanced-macros.setting.legacySlashCommand.name`,
-		hint: `advanced-macros.setting.legacySlashCommand.hint`,
-		scope: "world",
-		config: true,
-		default: false,
-		type: Boolean,
-	});
-	libWrapper.register(
-		"advanced-macros",
-		"Macro.prototype.canExecute",
-		function () {
-			if (!this.testUserPermission(game.user, "LIMITED")) return false;
-			return this.type === "script"
-				? game.user.can("MACRO_SCRIPT") || (canRunAsGM(this) && !game.user.isGM)
-				: true;
-		},
-		"OVERRIDE"
-	);
-	libWrapper.register(
-		"advanced-macros",
-		"Macro.prototype.execute",
-		async function (wrapped, scope = {}, callFromSocket = false) {
+	class AdvancedMacro extends CONFIG.Macro.documentClass {
+		static metadata = Object.freeze(mergeObject(super.metadata, {
+			preserveOnImport: ["_id", "sort", "ownership", "author"]
+		  }, {inplace: false}));
+
+		canUserExecute(user) {
+			if (!this.testUserPermission(user, "LIMITED")) return false;
+			return this.type === "script" ? user.can("MACRO_SCRIPT") || (canRunAsGM(this) && !user.isGM) : true;
+		}
+
+		async execute(scope = {}, callFromSocket = false) {
 			if (!this.canExecute) {
 				return ui.notifications.warn(`You do not have permission to execute Macro "${this.name}".`);
 			}
 			switch (this.type) {
 				case "chat":
-					return wrapped();
-				case "script":
+					return super.execute(scope);
+				case "script": {
 					const runFor = this.getFlag("advanced-macros", "runForSpecificUser");
 					if (callFromSocket || !runFor || runFor === "runAsWorldScript" || !canRunAsGM(this)) {
-						return wrapped(scope);
-					} else if (runFor == "GM") {
-						if (game.users.activeGM?.isSelf) {
-							return wrapped(scope);
-						}
+						return super.execute(scope);
+					} else if (runFor === "GM") {
+						if (game.users.activeGM?.isSelf) return super.execute(scope);
 						return socket.executeAsGM("executeMacro", this.id, scope);
-					} else if (runFor == "runForEveryone") {
+					} else if (runFor === "runForEveryone") {
 						return socket.executeForEveryone("executeMacro", this.id, scope);
-					} else if (runFor == "runForEveryoneElse") {
+					} else if (runFor === "runForEveryoneElse") {
 						return socket.executeForOthers("executeMacro", this.id, scope);
 					} else if (runFor) {
 						return socket.executeForUsers("executeMacro", [runFor], this.id, scope);
 					}
+				}
 			}
-		},
-		"MIXED"
-	);
-	libWrapper.register(
-		"advanced-macros",
-		"TextEditor._createContentLink",
-		(match, { async = false, relativeTo } = {}) => {
-			let [type, target, hash, name] = match.slice(1, 5);
+		}
+	}
+
+	class AdvancedTextEditor extends TextEditor {
+		static async _createContentLink(match, { relativeTo } = {}) {
+			const [type, target, hash, name] = match.slice(1, 5);
 
 			// Prepare replacement data
 			const data = {
-				cls: ["content-link"],
-				icon: null,
-				dataset: {},
-				name,
+				classes: ["content-link"],
+				attrs: { draggable: "true" },
+				dataset: { link: "" },
+				name
 			};
 
 			let doc;
 			let broken = false;
 			const scope = {};
 			if (type === "UUID") {
-				if (target.split(".")[0] == "Macro" && /\s/g.test(match[4])) {
+				if (target.split(".")[0] === "Macro" && /\s/g.test(match[4])) {
 					const macro = game.macros.get(target.split(".")[1]);
-					let params = match[4].replace(`${macro.name} `, "").split(" ");
+					const params = match[4].replace(`${macro.name} `, "").split(" ");
 					for (const p of params) {
 						const kv = p.split("=");
 						if (kv.length === 2) {
@@ -96,93 +80,70 @@ Hooks.once("init", () => {
 					}
 					data.name = macro.name;
 				}
-				data.dataset = { id: null, uuid: target, scope };
-				if (async) doc = fromUuid(target, { relative: relativeTo });
-				else {
-					try {
-						doc = fromUuidSync(target, { relative: relativeTo });
-					} catch (err) {
-						[type, ...target] = target.split(".");
-						broken = TextEditor._createLegacyContentLink(type, target.join("."), name, data);
-					}
-				}
+				Object.assign(data.dataset, {link: "", uuid: target, scope});
+				doc = await fromUuid(target, { relative: relativeTo });
 			} else broken = TextEditor._createLegacyContentLink(type, target, name, data);
 
-			// Flag a link as broken
-			if (broken) {
-				data.icon = "fas fa-unlink";
-				data.cls.push("broken");
+			if (doc) {
+				if ( doc.documentName ) return doc.toAnchor({ name: data.name, dataset: {
+					hash,
+					scope: Object.keys(data.dataset.scope),
+					...data.dataset.scope
+				} });
+				data.name = data.name || doc.name || target;
+      			const type = game.packs.get(doc.pack)?.documentName;
+				Object.assign(data.dataset, {type, id: doc._id, pack: doc.pack});
+				if (hash) data.dataset.hash = hash;
+				data.icon = CONFIG[type].sidebarIcon;
 			}
 
-			const constructAnchor = (doc) => {
-				if (doc) {
-					if (doc.documentName) {
-						const attrs = { draggable: true };
-						if (hash) attrs["data-hash"] = hash;
-						return doc.toAnchor({
-							attrs,
-							dataset: { scope: Object.keys(data.dataset.scope), ...data.dataset.scope },
-							classes: data.cls,
-							name: data.name,
-						});
-					}
-					data.name = data.name || doc.name || target;
-					const type = game.packs.get(doc.pack)?.documentName;
-					data.dataset.type = type;
-					data.dataset.id = doc._id;
-					data.dataset.pack = doc.pack;
-					if (hash) data.dataset.hash = hash;
-					data.icon = CONFIG[type].sidebarIcon;
-				} else if (type === "UUID") {
-					// The UUID lookup failed so this is a broken link.
-					data.icon = "fas fa-unlink";
-					data.cls.push("broken");
-				}
+			// The UUID lookup failed so this is a broken link.
+			else if (type === "UUID") broken = true;
 
-				const a = document.createElement("a");
-				a.classList.add(...data.cls);
-				a.draggable = true;
-				for (let [k, v] of Object.entries(data.dataset)) {
-					a.dataset[k] = v;
-				}
-				a.innerHTML = `<i class="${data.icon}"></i>${data.name}`;
-				return a;
-			};
+			// Broken links
+			if (broken) {
+				delete data.dataset.link;
+				delete data.attrs.draggable;
+				data.icon = "fas fa-unlink";
+				data.classes.push("broken");
+			}
+			return this.createAnchor(data);
+		}
 
-			if (doc instanceof Promise) return doc.then(constructAnchor);
-			return constructAnchor(doc);
-		},
-		"OVERRIDE"
-	);
-	libWrapper.register(
-		"advanced-macros",
-		"TextEditor._onClickContentLink",
-		async (event) => {
+		static async _onClickContentLink(event) {
 			event.preventDefault();
 			const doc = await fromUuid(event.currentTarget.dataset.uuid);
 			if (event.currentTarget.dataset.type !== "Macro") return doc?._onClickDocumentLink(event);
-			else {
-				const scope = {};
-				if (event.currentTarget.dataset.scope) {
-					const params = event.currentTarget.dataset.scope.split(",");
-					for (const p of params) {
-						scope[p] =
-							event.currentTarget.dataset[p] === "true" ||
-							(event.currentTarget.dataset[p] === "false" ? false : event.currentTarget.dataset[p]);
-					}
+
+			const scope = {};
+			if (event.currentTarget.dataset.scope) {
+				const params = event.currentTarget.dataset.scope.split(",");
+				for (const p of params) {
+					scope[p] =
+							event.currentTarget.dataset[p] === "true"
+							|| (event.currentTarget.dataset[p] === "false" ? false : event.currentTarget.dataset[p]);
 				}
-				// TODO set up logic check when to callFromSocket or not
-				return doc?.execute(scope);
 			}
-		},
-		"OVERRIDE"
-	);
-	Macro.metadata.preserveOnImport.push("author");
+			// TODO set up logic check when to callFromSocket or not
+			return doc?.execute(scope);
+		}
+	}
+
+	CONFIG.Macro.documentClass = AdvancedMacro;
+	TextEditor = AdvancedTextEditor;
+	game.settings.register("advanced-macros", "legacySlashCommand", {
+		name: "advanced-macros.setting.legacySlashCommand.name",
+		hint: "advanced-macros.setting.legacySlashCommand.hint",
+		scope: "world",
+		config: true,
+		default: false,
+		type: Boolean,
+	});
 });
 
 Hooks.on("chatMessage", (chatLog, message, chatData) => {
 	// Ignore messages starting with "<" or matching a macro pattern.
-	if (message.trim().startsWith("<") || message.match(chatLog.constructor.MESSAGE_PATTERNS["macro"])) return true;
+	if (message.trim().startsWith("<") || message.match(chatLog.constructor.MESSAGE_PATTERNS.macro)) return true;
 	if (!game.settings.get("advanced-macros", "legacySlashCommand")) return;
 	// If the message contains an invalid command and starts with a "/", try to process macros in it.
 	let [command, match] = chatLog.constructor.parse(message);
@@ -190,13 +151,13 @@ Hooks.on("chatMessage", (chatLog, message, chatData) => {
 		const messageArray = message.slice(1).split(" ");
 		let macroName = messageArray[0];
 		let macro = game.macros.getName(macroName);
-		messageArray.slice(1).forEach((token) => {
+		for (const token of messageArray.slice(1)) {
 			if (!macro) {
 				macroName += ` ${token}`;
 				macro = game.macros.getName(macroName);
 			}
-			if (macro) return;
-		});
+			if (macro) break;
+		}
 		if (macro) {
 			[command, match] = chatLog.constructor.parse(`/macro ${message.slice(1)}`);
 			chatLog._processMacroCommand(command, match);
@@ -263,13 +224,13 @@ Hooks.once("ready", () => {
 		});
 	});
 	const worldScripts = game.macros.contents.filter(
-		(macro) => getProperty(macro, `flags.advanced-macros.runForSpecificUser`) === "runAsWorldScript"
+		(macro) => macro.getFlag("advanced-macros", "runForSpecificUser") === "runAsWorldScript"
 	);
 	for (const macro of worldScripts) {
 		try {
 			macro.execute();
 			console.debug(`Advanced Macros | Executed "${macro.name}" world script (ID: ${macro.id})`);
-		} catch (err) {
+		} catch(err) {
 			console.error(`Advanced Macros | Error executing "${macro.name}" world script (ID: ${macro.id})`, err);
 		}
 	}
